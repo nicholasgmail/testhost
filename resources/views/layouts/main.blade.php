df24db5f3fa782f219fa7bb29617f982a43aa0f9
Nikolay Us <nicholas.us.03@gmail.com>
2020-10-23 00:40:00 +0300

    create: API search list movie

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,21 +19,12 @@
                                 d="M7 4v16M17 4v16M3 8h4m10 0h4M3 12h18M3 16h4m10 0h4M4 20h16a1 1 0 001-1V5a1 1 0 00-1-1H4a1 1 0 00-1 1v14a1 1 0 001 1z">
                             </path>
                         </svg>MovieApp</a></li>
-                <li class="md:ml-16 mt-3 md:mt-0"><a href="{{ route('index')}}" class="hover:text-gray-300"></a>Movies</li>
-                <li class="md:ml-6 mt-3 md:mt-0"><a href="#" class="hover:text-gray-300"></a>TV Shows</li>
-                <li class="md:ml-6 mt-3 md:mt-0"><a href="#" class="hover:text-gray-300"></a>Actors</li>
+                <li class="md:ml-16 mt-3 md:mt-0"><a href="{{ route('index')}}" class="hover:text-gray-300">Movies</a></li>
+                <li class="md:ml-6 mt-3 md:mt-0"><a href="#" class="hover:text-gray-300">TV Shows</a></li>
+                <li class="md:ml-6 mt-3 md:mt-0"><a href="#" class="hover:text-gray-300">Actors</a></li>
             </ul>
             <div class="flex flex-wrap item-center justify-center">
-                <div class="relative mt-3 md:mt-0">
-                    <input type="text"
-                        class="bg-gray-800 rounded-full text-sm w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
-                        placeholder="Search">
-                    <div class="absolute top-0"><svg class="w-4 h-4 mt-2 ml-2" fill="none" stroke="currentColor"
-                            viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg></div>
-                </div>
+                    <livewire:search-dropdown>
                 <div class="ml-4 mt-3 md:mt-0">
                     <a href="#"><img src="https://tailwindcss.com/img/jonathan.jpg" class="rounded-full w-8 h-8"
                             alt="avatar"></a>
@@ -42,6 +33,7 @@
         </div>
     </nav>
     @yield('content')
+    <livewire:scripts>
 </body>
 
 </html>
